fbfcf48cca6dec4afb67e1389db121fc38cc830b
Julian Kennedy <juliankenn@gmail.com>
2022-01-08 12:30:33 +0200

    started new directory layout

---
+++ apps/core/src/main/java/org/za/hash/glober/App.java
@@ -0,0 +1,13 @@
+package org.za.hash.glober;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
