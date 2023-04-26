43246a1a9d4bef55fef926383340f254bf03ef88
Julian Kennedy <juliankenn@gmail.com>
2023-04-26 23:52:49 +0200

    re-add

---
+++ spatialdata/src/main/java/io/terrexplor/spatialdata/SpatialDataMain.java
@@ -0,0 +1,13 @@
+package io.terrexplor.spatialdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class SpatialDataMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from Spatial" );
+    }
+}
